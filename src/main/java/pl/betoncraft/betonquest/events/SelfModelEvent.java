f0d0022971c33f0cd429a4eda60e760e65715f9b
justwei <admin@taylorswiftcn.com>
2021-12-22 15:56:01 +0800

    [update] SelfModelEvent

---
+++ src/main/java/pl/betoncraft/betonquest/events/SelfModelEvent.java
@@ -0,0 +1,25 @@
+package pl.betoncraft.betonquest.events;
+
+import net.sakuragame.eternal.justmessage.screen.ui.conversation.ConversationScreen;
+import pl.betoncraft.betonquest.api.event.QuestEvent;
+import pl.betoncraft.betonquest.core.Instruction;
+import pl.betoncraft.betonquest.exceptions.InstructionParseException;
+import pl.betoncraft.betonquest.exceptions.QuestRuntimeException;
+import pl.betoncraft.betonquest.utils.PlayerConverter;
+
+import java.util.UUID;
+
+public class SelfModelEvent extends QuestEvent {
+
+    private final double scale;
+
+    public SelfModelEvent(Instruction instruction) throws InstructionParseException {
+        super(instruction);
+        this.scale = Double.parseDouble(instruction.next());
+    }
+
+    @Override
+    public void run(UUID uuid) throws QuestRuntimeException {
+        ConversationScreen.setConvNPC(PlayerConverter.getPlayer(uuid), uuid, scale);
+    }
+}
